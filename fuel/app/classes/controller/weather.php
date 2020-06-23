759e21c97d3a5a2811e3dccf85ab19fd68257db6
root <root@localhost.localdomain>
2020-06-23 13:31:27 +0900

    Initial Commit

---
+++ fuel/app/classes/controller/weather.php
@@ -0,0 +1,49 @@
+<?php
+
+class Controller_Weather extends Controller_Template
+{
+
+	public function get_top()
+	{
+		//$data["subnav"] = array('get'=> 'active' );
+		//$this->template->title = 'Weather &raquo; Get';
+		//$this->template->content = View::forge('weather/get', $data);
+		$hello = array();
+		$hello['value'] = 'Hello'; 
+		return View::forge('weather/get',$hello);
+	}
+
+	public function post_city()
+	{
+		$content = \Input::json('city');
+		$curl = \Request::forge('https://api.openweathermap.org/data/2.5/weather?', 'curl');
+		$curl->set_method('get');
+		$curl->set_params(array (
+			    'q' => $content,
+			    'units' => 'metric',
+			    'appid' => 'e753db02de38e5d06f1fdb54a251f5d1'
+			));
+		$weather = json_decode($curl->execute()->response());
+		$weather_json = json_encode(array(
+			    'weather' => $weather->weather[0]->main,
+			    'temp' => $weather->main->temp,
+			    'wind' => $weather->wind->speed
+			));
+		// $base = "https://api.openweathermap.org/data/2.5/weather?";
+        // $query = array (
+        //     'q' => $content,
+        //     'units' => 'metric',
+        //     'appid' => 'e753db02de38e5d06f1fdb54a251f5d1'
+        // );
+		// $url = $base . http_build_query($query);
+		// $weather = json_decode(file_get_contents($url), true);
+        // $weather_json = json_encode(array(
+        //     'weather' => $weather['weather'][0]['main'],
+        //     'temp' => $weather['main']['temp'],
+        //     'wind' => $weather['wind']['speed']
+        // ));
+        return $weather_json;
+
+	}
+
+}
