5ca46b54c9b6058d76cd77f336d324c3952e5ee4
root <root@localhost.localdomain>
2020-06-26 18:22:54 +0900

    fix/form

--- fuel/app/views/word/form.php
+++ fuel/app/views/word/form.php
@@ -1,11 +1,9 @@
-<html>
-    <meta charset="UTF-8">
-    <body>
-        <form method="POST" action=<?php echo $content == null ? "/word/create" : "/word/edit/" . $id; ?>>
-            <label>word：</label>
-            <input type="text" name="content" <?php echo $content !== null ? 'value=' . $content : "" ; ?>>
-            <input type="submit" value="送信">
-            <a href="/word">back</a>
-        </form>  
-    </body>
-</html>
+
+
+<?php echo Form::open(array('method' => 'post')); ?>
+<fieldset>
+    <?php echo Form::label('Word','word'); ?>
+    <?php echo Form::input('content', isset($content) ? $content : ''); ?> 
+    <?php echo Form::submit('submit', 'Save'); ?>
+</fieldset>
+<?php echo Form::close(); ?>
