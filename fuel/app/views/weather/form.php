f15604c7a2eb4c3df454d22d9357b36d61e3c2b7
root <root@localhost.localdomain>
2020-06-30 12:30:12 +0900

    feature/request-post-form

--- fuel/app/views/weather/form.php
+++ fuel/app/views/weather/form.php
@@ -2,3 +2,13 @@
 	<p>Hello</p>
 	<button type="button" class="btn btn-primary">Click</button>
 </div>
+
+<div class="container">
+	<form action="weather" method="POST">
+		<div class="form-group">
+			<label for="city">Where?</label>
+			<input type="text" class="form-control" id="city" name="city" placeholder="location">
+		</div>
+		<button type="submit" class="btn btn-primary">Submit</button>
+	</form>
+</div>
